267ffbe2905ead80263371bc09a36eb1fa640b0b
louisvno <louisvn@gmail.com>
2016-12-02 20:45:23 +0100

    Extract discount functions

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -22,39 +22,62 @@ public class ShoppingCart {
     
     //output the contents of the cart
     public String toString() {
-        return "Your cart contains:" + this.productsInCart + " total price = " + getTotalPrice();
+        return "Your cart contains:" + this.productsInCart +
+                " Total price = " + getTotalPrice() +
+                " Discount = " +
+                " Final amount = " +;
     }
-    
-    //calculate total price of cart content ArrayList<Product> p
-    double getTotalPrice(){
-        double totalPrice = 0.0;
-        int numberOfProducts = this.productsInCart.size();
+
+    //get count of each product added to Shoppingcart
+    private HashMap <String, Integer> getProductCount() {
         HashMap <String, Integer> productsCounter = new HashMap<>();
         String productName;
         Integer productCount;
+        int numberOfProducts = getNumberOfProducts();
 
-        for (int i = 0; i < numberOfProducts; i++){
+        for (int i = 0; i < numberOfProducts; i++) {
             //get product name
             productName = this.productsInCart.get(i).name;
-            //check if the productname exists in the hashmap
-            if (productsCounter.containsKey(productName)){
+            //check if the product name exists in the hash map
+            if (productsCounter.containsKey(productName)) {
                 productCount = productsCounter.get(productName);
                 productsCounter.put(productName, (productCount + 1));
-            }else {
+            } else {
                 productsCounter.put(productName, 1);
-            };
-
-            totalPrice += this.productsInCart.get(i).price;
+            }
         }
+        return productsCounter;
+    }
 
-        System.out.println(productsCounter);
+    //calculate discounts
+    private double getDiscountOverFiveItems () {
+        double totalPrice = getTotalPrice();
+        int numberOfProducts = getNumberOfProducts();
 
         if (numberOfProducts >= 5){
-            System.out.println("original price was" + totalPrice + ", 10% discount applied!");
-            return totalPrice*0.9;
-        } else { 
-            return totalPrice;
-
+            return totalPrice*0.1;
+        } else {
+            return 0.0;
         }
+    }
+
+    private double getDiscountFourthItem(){
+        HashMap <String, Integer> productCount = getProductCount();
+
+    }
+
+    //calculate total price of cart content ArrayList<Product>
+    private double getTotalPrice(){
+        double totalPrice = 0.0;
+        int numberOfProducts = getNumberOfProducts();
+
+        for (int i = 0; i < numberOfProducts; i++) {
+            totalPrice += this.productsInCart.get(i).price;
+        }
+       return totalPrice;
+    }
+
+    private int getNumberOfProducts(){
+        return this.productsInCart.size();
     }
 }
