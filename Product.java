f17e05f477eee1d84bea5724e1c1b25e06503527
louisvno <louisvn@gmail.com>
2016-12-04 16:47:56 +0100

    Add Discount for every fourth eight etc. item

--- Product.java
+++ Product.java
@@ -5,18 +5,17 @@ public class Product {
     public static void main(String [ ] args){
     
         //TYPE,  NAME =  new  call CONSTRUCTOR
-        Product p1 = new Product("TV" , 999.95);
-        Product p2 = new Product("Playstation", 399.95);
-        System.out.println(p1.toString());
-        
+        Product TV = new Product("TV" , 999.95);
+        Product playstation = new Product("Playstation", 399.95);
+
         ShoppingCart my_cart = new ShoppingCart();
         
-        my_cart.addProduct(p1);
-        my_cart.addProduct(p2);
-        my_cart.addProduct(p2);
-        my_cart.addProduct(p2);
-        my_cart.addProduct(p2);
-        my_cart.addProduct(p2);
+        my_cart.addProduct(TV);
+        my_cart.addProduct(playstation);
+        my_cart.addProduct(playstation);
+        my_cart.addProduct(playstation);
+        my_cart.addProduct(playstation);
+        my_cart.addProduct(playstation);
         System.out.println(my_cart.toString());
 }
     
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -7,39 +7,63 @@ public class ShoppingCart {
     ArrayList<Product> productsInCart;
     int numberOfProducts;
     double totalPrice;
+    double discountFivePlusProducts;
+    double discountFourthProduct;
     double totalDiscount;
     double finalPrice;
 
     //**Initialize the properties with Constructor
     ShoppingCart(){
         this.productsInCart = new ArrayList<Product>();
+        this.totalPrice = 0.0;
+        this.discountFivePlusProducts = 0.0;
+        this.discountFourthProduct = 0.0;
+        this.totalDiscount = 0.0;
+        this.finalPrice = 0.0;
     }
     
-    /*Methods*/
+    /*Set the Methods*/
+    private void updateCart() {
+        setNumberOfProducts();
+        setTotalPrice();
+        setDiscountFourthProduct();
+        setDiscountFivePlusProducts();
+        setTotalDiscount();
+        setFinalPrice();
+    }
 
     //add a product to the ArrayList productsInCart (ShoppingCart)
     public void addProduct(Product product) {
         this.productsInCart.add(product);
+        updateCart();
         System.out.println(product.name + " added to cart");
-        setCart();
     }
-    
+
     //output the contents of the cart
     public String toString() {
         return "Your cart contains:" + this.productsInCart +
-                " Total price = " + getTotalPrice() +
-                " Discount = " +
-                " Final amount = " +;
+                " Total price = " + this.totalPrice +
+                " Discount = " + this.totalDiscount +
+                " Final amount = " + this.finalPrice;
     }
 
-    //get count of each product added to Shoppingcart
+    //get price of products
+    private HashMap<String , Double> getProductPrices(){
+        HashMap<String, Double> productPrices = new HashMap<>();
+
+        this.productsInCart.forEach((Product)->{
+            productPrices.put(Product.name,Product.price);
+        });
+        return productPrices;
+    }
+
+    //get count of all products added to Shoppingcart
     private HashMap <String, Integer> getProductCount() {
         HashMap <String, Integer> productsCounter = new HashMap<>();
         String productName;
         Integer productCount;
-        int numberOfProducts = getNumberOfProducts();
 
-        for (int i = 0; i < numberOfProducts; i++) {
+        for (int i = 0; i < this.numberOfProducts; i++) {
             //get product name
             productName = this.productsInCart.get(i).name;
             //check if the product name exists in the hash map
@@ -54,34 +78,49 @@ public class ShoppingCart {
     }
 
     //calculate discounts
-    private double getDiscountOverFiveItems () {
-        double totalPrice = getTotalPrice();
-        int numberOfProducts = getNumberOfProducts();
+    private void setDiscountFivePlusProducts () {
+        double totalPrice = this.totalPrice;
 
-        if (numberOfProducts >= 5){
-            return totalPrice*0.1;
+        if (this.numberOfProducts >= 5){
+            this.discountFivePlusProducts = totalPrice*0.1;
         } else {
-            return 0.0;
+            this.discountFivePlusProducts = 0.0;
         }
     }
 
-    private double getDiscountFourthItem(){
-        HashMap <String, Integer> productCount = getProductCount();
+    private void setDiscountFourthProduct() {
+        HashMap<String, Integer> productCount = getProductCount();
+        HashMap<String, Double> productPrices = getProductPrices();
 
+            //Fer every element of TYPE > pass variable  :  map entries
+        for(HashMap.Entry<String,Integer> entry : productCount.entrySet()){
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            if (v % 4 == 0) {
+                this.discountFourthProduct += (v/4) * productPrices.get(k);
+            }
+        };
     }
-
+    
+    private void setTotalDiscount(){
+       this.totalDiscount = this.discountFivePlusProducts + this.discountFourthProduct;
+    }
+    
     //calculate total price of cart content ArrayList<Product>
-    private double setTotalPrice(){
+    private void setTotalPrice(){
         double totalPrice = 0.0;
-        int numberOfProducts = getNumberOfProducts();
 
-        for (int i = 0; i < numberOfProducts; i++) {
+        for (int i = 0; i < this.numberOfProducts; i++) {
             totalPrice += this.productsInCart.get(i).price;
         }
-       return totalPrice;
+       this.totalPrice = totalPrice;
     }
 
-    private int getNumberOfProducts(){
-        return this.productsInCart.size();
+    private void setNumberOfProducts(){
+        this.numberOfProducts = this.productsInCart.size();
+    }
+
+    private void setFinalPrice(){
+        this.finalPrice = this.totalPrice - this.totalDiscount;
     }
 }
